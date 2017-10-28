4fc7c5143afc4461761105e7a60ba491dbe32788
sayarattar <33138821+sayarattar@users.noreply.github.com>
2017-10-27 20:50:24 -0400

    Update UserLibrary.cpp

--- Assignment_2/UserLibrary.cpp
+++ Assignment_2/UserLibrary.cpp
@@ -3,32 +3,39 @@
 using namespace std;
 
 UserLibrary::UserLibrary(){
-  vector<UserLibrary*> library;
+    vector<User*> library;
 }
 
-~UserLibrary::UserLibrary(){
-  for(int i=0; i<library.size(); i++){
-    delete library[i];
-  }
+UserLibrary::~UserLibrary(){
+    for(unsigned int i = 0; i< library.size(); ++i){
+        delete library[i];
+    }
 }
 
-  void UserLibrary::addUser ( User* user){
+void UserLibrary::addUser(User* user){
     library.push_back(user);
-  }
+}
 
-  void UserLibrary::removeUser( User* user){
-    for(int i=0; i<library.size(); i++){
-      User * temp=library[i];
-      if(temp==user){
-        library.erase(library.begin()+i);
-        delete temp;
-        return;
-      }
+void UserLibrary::displayUsers(void){
+    for(unsigned int i = 0; i < library.size(); ++i){
+        library[i] -> displayUser();
     }
-  }
+}
 
-  void UserLibrary::displayUsers(void){
-    for(int i=0; i<library.size(); i++){
-      cout<<library[i]<<endl;
+void UserLibrary::removeUser(User* user){
+    for(unsigned int i = 0; i < library.size(); ++i){
+        User * temp = library[i];
+        if(temp == user){
+            library.erase(library.begin() + i);
+            delete temp;
+        }
     }
-  }
+}
+
+User * UserLibrary::getUser(int user_id){
+    for(unsigned int i = 0; i < library.size(); ++i){
+          User * temp = library[i];
+          if(temp -> getUserID() == user_id) return temp;
+    }
+    return NULL;
+}
