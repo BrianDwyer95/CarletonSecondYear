9f0e93912beccc9b678e6bed0c691629bcd037bd
sayarattar <33138821+sayarattar@users.noreply.github.com>
2017-10-27 20:51:22 -0400

    Update User.cpp

--- Assignment_2/User.cpp
+++ Assignment_2/User.cpp
@@ -1,29 +1,30 @@
 #include "User.h"
+#include <iostream>
 
 using namespace std;
 
 User::User(int user_id, string user_name){
-  this -> user_id=user_id;
-  this -> user_name=user_name;
-}
-
-User::~User(){
-  cout<<"Destruct User"<<endl;
+    this -> user_id = user_id;
+    this -> user_name = user_name;
 }
 
 User::User(User& user){
-  user_id=user.user_id;
-  user_name=user.user_name;
+    user_id = user.user_id;
+    user_name= user.user_name;
 }
 
-string User::getUser_name(void){
-  return user_name;
+User::~User(){
+ cout << "Destruct User" << endl;
 }
 
-int User::getUser_id(void){
-  return user_id;
+int User::getID(void){
+    return user_id;
+}
+
+string User::getName(void){
+    return user_name;
 }
 
 void User::displayUser(void){
-  cout<<"User ID: "<<getUser_name()<<" User name: "<<getUser_id()<<endl;
+    cout << "User Name: " << user_name << " User ID: " << user_id << endl;
 }
