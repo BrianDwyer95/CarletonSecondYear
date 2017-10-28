b2073ebae8bff8725e333122bb055c1586336532
sayarattar <33138821+sayarattar@users.noreply.github.com>
2017-10-27 20:53:50 -0400

    Update User.h

--- Assignment_2/User.h
+++ Assignment_2/User.h
@@ -1,17 +1,25 @@
-#include <iostream>
-#include <vector>
-
+#include <string>
 
 using namespace std;
 
 class User{
-public:
-  User(int user_id, string user_name);
-  ~User();
-  User(User& user);
-  void getUser_id(void);
-  void getUser_name(void);
-private:
-  string user_name;
-  int user_id;
+    public:
+        User(int user_id, string user_name);
+        User(User& user);
+        User & operator=(const User & user) {
+            if(&user != this) {
+               delete &this -> user_id;
+               delete &this -> user_name;
+               this -> user_name = user.user_name;
+               this -> user_id = user.user_id;
+            }
+            return *this;
+        }
+        ~User();
+        string getName(void);
+        int getID(void);
+        void displayUser(void);
+    private:
+        int user_id;
+        string user_name;
 };
